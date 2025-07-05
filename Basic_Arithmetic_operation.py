8237cfbebc203ea7c1c2c66123bfc95e84b28f12
Code With Darryll <pajaganasdarryll2004@gmail.com>
2025-07-05 18:45:49 +0800

    Create Basic_Arithmetic_operation.py

---
+++ Basic_Arithmetic_operation.py
@@ -0,0 +1,58 @@
+#->Math 
+#->Arithmetic Operators
+#->Math Functions
+
+#Basic Arithmetic Operators
+#Addition
+friends = 0
+friends = friends + friends
+
+#Augmented assignment operator example
+
+friends1 = 0
+friends1 +=1
+
+print(friends)
+print(friends1)
+
+# Subtraction
+friends3 = 10
+friends4 = 10
+friends3 = friends3 - 2
+friends4 -=1
+print(friends3)
+print(friends4)
+
+#Multiplication
+friends5 = 10
+friends6 = 10
+friends5 = friends5 * 2
+friends6 *=1
+print(friends5)
+print(friends6)
+
+#Division
+friends7 = 10
+friends8 = 10
+friends7 = friends7 / 2
+friends8 /=2
+print(friends7)
+print(friends8)
+
+#exponent
+friendsa = 10
+friendsb = 10
+friendsa = friendsa ** 2
+friendsb **=2
+print(friendsa)
+print(friendsb)
+
+#remainder
+friendsa1 = 10
+friendsb2 = 10
+friendsa1 = friendsa1 % 4
+friendsb2 %=4
+print(friendsa1)
+print(friendsb2)
+
+
