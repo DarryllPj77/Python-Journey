ad5caf709dd393b1bcd83296cf45e42a44d623a6
Code With Darryll <pajaganasdarryll2004@gmail.com>
2025-07-05 17:41:48 +0800

    Create Sample_castingwith_input.py

---
+++ Sample_castingwith_input.py
@@ -0,0 +1,9 @@
+#Type casting with user input
+name = input("Enter your name: ")
+age =float(input("Enter your  age: "))
+
+#age = int(age)                                       
+age = age + 1
+
+print(f"Hello {name}")
+print(f"Your are {age} years old")
