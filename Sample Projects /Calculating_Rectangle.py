ac11c4ec57cf55540f143ec8cf593369cb8796d2
Code With Darryll <pajaganasdarryll2004@gmail.com>
2025-07-05 17:32:57 +0800

    Create Calculating_Rectangle.py

---
+++ Sample Projects /Calculating_Rectangle.py
@@ -0,0 +1,9 @@
+#Calculating the area and volume of an rectangle
+length = float(input("Enter the length of a rectangle: "))
+width = float(input("Enter the width of a rectangle: "))
+height = float(input("Enter the height of a rectangle: "))
+volume = length * width * height # if it is a 3D Rectangle
+area = length * width # if it is a 2D Rectangle
+print(f"The are of the Rectangle is {volume} cm^2 ") 
+print(f"The are of the Rectangle is {area} cm^2 ")
+print(f"The are of the Rectangle is {volume} cm^2 ")
