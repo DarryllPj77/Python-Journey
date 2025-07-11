97a7be08c1a3d5d3186b62689cf2f3012c03c4df
Code With Darryll <pajaganasdarryll2004@gmail.com>
2025-07-11 08:07:17 +0800

    Create if_statement.py

---
+++ if_statement.py
@@ -0,0 +1,56 @@
+'''
+Example #1
+age = int(input("Enter you're Age:"))
+if age >= 100:
+    print("You are too old to sign up!")
+elif age >= 18:
+    print("You are now signed up!")
+elif age < 0:
+    print("You haven't born yet!")
+else:
+    print("You must be 18+ to sign up")
+'''
+
+'''
+Example #2
+response = input("Would you like food? (Y/N): ")
+
+if response == "Y" and "y":
+    print("Have some food!")
+else:
+    print("No food for you!")
+'''
+'''
+Example #3
+name = input("Enter you're name!: ")
+if name == "":
+    print("You did not type in you're name!")
+else:
+    print(f"Hello {name}")
+'''
+
+Example #4
+for_sale = input("Enter if TRUE or FALSE: ").strip().lower()
+
+if for_sale == "true" and "FALSE":
+    print("This item is For Sale!")
+elif for_sale == "false":
+    print("This item is NOT For Sale!")
+else:
+    print("You entered a value that is not in the choices.")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
